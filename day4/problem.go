759a4b2fde9e5f0ecc5dd5e2cc1119a68cd05d97
Brian Celenza <bri@celenza.dev>
2023-12-16 10:57:37 -0800

    Add Day 4 Part 2

--- day4/problem.go
+++ day4/problem.go
@@ -7,15 +7,21 @@ import (
 	"strings"
 )
 
-func Solve(input string) int {
+// Solve Day 4, returning a tuple of (part 1 total score, part 2 total cards).
+func Solve(input string) (int, int) {
 	// Create a variable to keep track of the score for all cards.
 	var totalScore = 0
 
 	// Split the input into distinct cards by line.
 	cards := strings.Split(input, "\n")
 
+	// For part 2, we'll need to keep track of the number of matches for each
+	// card so we can calculate the total number of cards we'd have at the end.
+	cardMatches := make([]int, len(cards))
+	cardCount := make([]int, len(cards))
+
 	// Split the cards into their respective sets (winners and numbers we have).
-	for _, card := range cards {
+	for cardIdx, card := range cards {
 		// Create a variable to keep track of the score for this one card.
 		var score = 0
 
@@ -33,6 +39,12 @@ func Solve(input string) int {
 		// to keep track of the number of winners that happen to be the same
 		// number in case its actually important.
 		winners := make(map[int]int)
+
+		// For part 2, we'll need to keep track of how many matches we have and
+		// what card number we're on. We can also go ahead and increase the card
+		// count for this card.
+		var matches = 0
+		cardCount[cardIdx]++
 
 		// Begin reading the parsed symbols, omitting the first because its the
 		// card number.
@@ -63,6 +75,7 @@ func Solve(input string) int {
 			} else {
 				// Otherwise, we're scoring ourselves.
 				if _, isWinner := winners[num]; isWinner {
+					matches++
 					if score == 0 {
 						score = 1
 					} else {
@@ -72,8 +85,28 @@ func Solve(input string) int {
 			}
 		}
 
+		// For part 2, store the number of matches for this card, which we'll
+		// use to calculate the total number of cards.
+		cardMatches[cardIdx] = matches
+
+		// Add this score to the total score for part 1
 		totalScore += score
 	}
 
-	return totalScore
+	// To calculate the total number of cards, we first need to increase
+	// the card counts to take into account duplicated cards from the previous
+	// card's number of matching winners.
+	for cardIdx, matches := range cardMatches {
+		for i := 0; i < matches; i++ {
+			cardCount[cardIdx+i+1] += cardCount[cardIdx]
+		}
+	}
+
+	// Finally, sum the total number of cards.
+	var totalCards = 0
+	for _, cardCount := range cardCount {
+		totalCards += cardCount
+	}
+
+	return totalScore, totalCards
 }
--- day4/problem_test.go
+++ day4/problem_test.go
@@ -206,9 +206,27 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
-	assert.Equal(t, 13, Solve(input))
+	score, _ := Solve(input)
+	assert.Equal(t, 13, score)
 }
 
 func TestSolvePart1_Real(t *testing.T) {
-	assert.Equal(t, 21213, Solve(realInput))
+	score, _ := Solve(realInput)
+	assert.Equal(t, 21213, score)
+}
+
+func TestSolvePart2_Practice(t *testing.T) {
+	input := `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
+	_, totalCards := Solve(input)
+	assert.Equal(t, 30, totalCards)
+}
+
+func TestSolvePart2_Real(t *testing.T) {
+	_, totalCards := Solve(realInput)
+	assert.Equal(t, 8549735, totalCards)
 }
